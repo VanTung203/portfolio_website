78cc5af45b6193855e161a71c72b22ad9abf7c3a
Van Tung <phutung06@gmail.com>
2025-05-08 23:37:01 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-blue-500 text-white p-4">
+      <h1 className="text-3xl font-bold">Chào mừng đến với Portfolio!</h1>
+      <p className="mt-2">Đây là một ví dụ sử dụng TailwindCSS trong React.</p>
     </div>
   );
 }
